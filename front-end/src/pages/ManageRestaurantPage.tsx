806621a76f8c9f56cdfb89eed17b5c109dcb42e9
Jagadeesh <jagadeeshpodilapu04@gmail.com>
2024-07-19 12:22:42 +0530

    Update ManageRestaurantPage.tsx

--- front-end/src/pages/ManageRestaurantPage.tsx
+++ front-end/src/pages/ManageRestaurantPage.tsx
@@ -1,12 +1,10 @@
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
-import React from "react";
+
 
 const ManageRestaurantPage = () => {
   return (
     <ManageRestaurantForm
-      onSave={function (restaurantFormData: FormData): void {
-        throw new Error("Function not implemented.");
-      }}
+      onSave={}
       isLoading={false}
     />
   );
