fa84e6942bac645ccf056b448046ab98fe2735f0
Jagadeesh <jagadeeshpodilapu04@gmail.com>
2024-07-19 12:25:45 +0530

    Update ManageRestaurantForm.tsx

--- front-end/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ front-end/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -43,7 +43,7 @@ const formSchema = z.object({
 type restaurantFormData = z.infer<typeof formSchema>;
 
 type Props = {
-  onSave: (restaurantFormData: FormData) => void;
+ 
   isLoading: boolean;
 };
 
