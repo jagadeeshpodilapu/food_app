c383b6df8e9731a10711a5362e0396d1207d282d
Jagadeesh <jagadeeshpodilapu04@gmail.com>
2024-07-19 12:20:47 +0530

    Update MenuSection.tsx

--- front-end/src/forms/manage-restaurant-form/MenuSection.tsx
+++ front-end/src/forms/manage-restaurant-form/MenuSection.tsx
@@ -22,7 +22,7 @@ const MenuSection = () => {
             <FormField
                 control={control}
                 name="menuItems"
-                render={({ field }) => (
+                render={({  }) => (
                     <FormItem className="flex flex-col gap-2">
                         {fields.map((_, index) => (
                             <MenuItemInput
