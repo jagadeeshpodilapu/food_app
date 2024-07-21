54c1c99e28af369189894e9483e577889ec75b83
Jagadeesh <jagadeeshpodilapu04@gmail.com>
2024-07-21 19:05:51 +0530

    deploy  issues solved

--- front-end/src/forms/manage-restaurant-form/ImageSection.tsx
+++ front-end/src/forms/manage-restaurant-form/ImageSection.tsx
@@ -1,54 +1,53 @@
 import {
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormMessage,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useFormContext } from "react-hook-form";
 
 const ImageSection = () => {
-  const { control, watch } = useFormContext();
+    const { control } = useFormContext();
 
-  const existingImageUrl = watch("imageUrl");
 
-  return (
-    <div className="space-y-2">
-      <div>
-        <h2 className="text-2xl font-bold">Image</h2>
-        <FormDescription>
-          Add an image that will be displayed on your restaurant listing in the
-          search results. Adding a new image will overwrite the existing one.
-        </FormDescription>
-      </div>
+    return (
+        <div className="space-y-2">
+            <div>
+                <h2 className="text-2xl font-bold">Image</h2>
+                <FormDescription>
+                    Add an image that will be displayed on your restaurant listing in the
+                    search results. Adding a new image will overwrite the existing one.
+                </FormDescription>
+            </div>
 
-      <div className="flex flex-col gap-8 md:w-[50%]">
+            <div className="flex flex-col gap-8 md:w-[50%]">
 
-        <FormField
-          control={control}
-          name="imageFile"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input
-                  className="bg-white"
-                  type="file"
-                  accept=".jpg, .jpeg, .png"
-                  onChange={(event) =>
-                    field.onChange(
-                      event.target.files ? event.target.files[0] : null
-                    )
-                  }
+                <FormField
+                    control={control}
+                    name="imageFile"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormControl>
+                                <Input
+                                    className="bg-white"
+                                    type="file"
+                                    accept=".jpg, .jpeg, .png"
+                                    onChange={(event) =>
+                                        field.onChange(
+                                            event.target.files ? event.target.files[0] : null
+                                        )
+                                    }
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
                 />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-      </div>
-    </div>
-  );
+            </div>
+        </div>
+    );
 };
 
 export default ImageSection;
